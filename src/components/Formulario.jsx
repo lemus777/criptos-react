0a5d0e063cc5bc69fb3f9fbc747f3fc6a0bf187b
lemus <lemus777@gmail.com>
2023-07-17 17:50:38 +0200

    monedas y api a criptomonedas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react"
 import styled from "@emotion/styled"
+import useSelectMonedas from "../hooks/useSelectMonedas"
+import { monedas } from "../data/monedas"
 
 const InputSubmit = styled.input`
   background-color: #9497FF;
@@ -10,6 +13,7 @@ const InputSubmit = styled.input`
   text-transform: uppercase;
   font-size: 20px;
   border-radius: 5px;
+  margin-top: 30px;
   transition: background-color .3s ease;
   &:hover {
     background-color: #7A7DFE;
@@ -18,9 +22,25 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
+  // useSelectMonedas nos retorna un arreglo con dos elementos
+  // el nombre del primer elemento de nuestro array hará referencia al primer elemento del arreglo retornado, y el del segundo al segundo
+  // por eso podemos llamarlo como queramos sin necesitar que coincida el nombre
+  const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu moneda', monedas) 
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
+      const respuesta = await fetch(url)
+      const resultado = await respuesta.json()
+      console.log(resultado.Data)
+
+    }
+    consultarAPI()
+  }, [])
+
   return (
     <form>
-
+      <SelectMonedas />
       <InputSubmit
         type="submit"
         value='Cotizar'
